5a5d7d2044bea31e4a16765f747f88e218f10ef2
LITU-VAI <sabbirkhan2m@gmail.com>
2025-06-19 00:31:35 +0600

    Update 1k.py

--- 1k.py
+++ 1k.py
@@ -417,25 +417,19 @@ class _G_i_F_t_:
                 'api_key':'882a8490361da98702bf97a021ddc14d',
                 'access_token':accessToken,}
                 headers = {
-    'authority': 'm.facebook.com',
+    'authority': 'web.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     'cache-control': 'max-age=0',
-    'dpr': '2.1000001430511475',
-    'sec-ch-prefers-color-scheme': 'dark',
     'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
-    'sec-ch-ua-full-version-list': '"Chromium";v="137.0.7337.0", "Not/A)Brand";v="24.0.0.0"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-model': '"POCO M2 Pro"',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-ch-ua-platform-version': '"12.0.0"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
+    'sec-fetch-site': 'same-site',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
-    'viewport-width': '980',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
 }
                 url = 'https://graph.facebook.com/auth/login'
                 po = requests.post(url,data=data,headers=headers).json()
@@ -499,21 +493,15 @@ class _G_i_F_t_:
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     'cache-control': 'max-age=0',
-    'dpr': '2.1000001430511475',
-    'sec-ch-prefers-color-scheme': 'dark',
     'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
-    'sec-ch-ua-full-version-list': '"Chromium";v="137.0.7337.0", "Not/A)Brand";v="24.0.0.0"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-model': '""',
     'sec-ch-ua-platform': '"Linux"',
-    'sec-ch-ua-platform-version': '""',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
     'sec-fetch-site': 'same-site',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
-    'viewport-width': '980',
 }
                 url = 'https://b-graph.facebook.com/auth/login'
                 po = requests.post(url,data=data,headers=headers).json()
