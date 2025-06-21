a2da8bd4767ccd8055b4f2eb6851472f09d53681
LITU-VAI <sabbirkhan2m@gmail.com>
2025-06-21 23:14:33 +0600

    Update 1k.py

--- 1k.py
+++ 1k.py
@@ -416,13 +416,13 @@ class _G_i_F_t_:
                 'fb_api_req_friendly_name':'authenticate',
                 'api_key':'882a8490361da98702bf97a021ddc14d',
                 'access_token':accessToken,}
-                headers =  {
-    'authority': 'm.facebook.com',
-     'method':'GET',
+                headers = {
+    'authority': 'x.facebook.com',
+    'method':'GET',
     'path':'/login/device-based/regular/login/?refsrc=deprecated&lwv=101&ref=dbl',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'cache-control': 'max-age=0'
+    'cache-control': 'max-age=0',
     'dpr': '2.1000001430511475',
     'sec-ch-prefers-color-scheme': 'dark',
     'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
@@ -439,7 +439,7 @@ class _G_i_F_t_:
     'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
     'viewport-width': '980',
 }
-                url = 'https://m.facebook.com/auth/login'
+                url = 'https://graph.facebook.com/auth/login'
                 po = requests.post(url,data=data,headers=headers).json()
                 if "session_key" in po:
                     uid = po['uid']
@@ -497,29 +497,29 @@ class _G_i_F_t_:
                 'client_country_code':'GB',
                 'fb_api_req_friendly_name':'authenticate'}
                 headers = {
-               'authority': 'm.facebook.com',
-               'method':'GET',
-               'path':'/login/device-based/regular/login/?refsrc=deprecated&lwv=101&ref=dbl',
-               'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-               'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-               'cache-control': 'max-age=0'
-               'dpr': '2.1000001430511475',
-               'sec-ch-prefers-color-scheme': 'dark',
-               'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
-               'sec-ch-ua-full-version-list': '"Chromium";v="137.0.7337.0", "Not/A)Brand";v="24.0.0.0"',
-               'sec-ch-ua-mobile': '?1',
-               'sec-ch-ua-model': '"POCO M2 Pro"',
-               'sec-ch-ua-platform': '"Android"',
-               'sec-ch-ua-platform-version': '"12.0.0"',
-               'sec-fetch-dest': 'document',
-               'sec-fetch-mode': 'navigate',
-               'sec-fetch-site': 'same-origin',
-               'sec-fetch-user': '?1',
-               'upgrade-insecure-requests': '1',
-               'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
-               'viewport-width': '980',
-		}
-                url = 'https://m.facebook.com/auth/login'
+    'authority': 'x.facebook.com',
+    'method':'GET',
+    'path':'/login/device-based/regular/login/?refsrc=deprecated&lwv=101&ref=dbl',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'cache-control': 'max-age=0',
+    'dpr': '2.1000001430511475',
+    'sec-ch-prefers-color-scheme': 'dark',
+    'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
+    'sec-ch-ua-full-version-list': '"Chromium";v="137.0.7337.0", "Not/A)Brand";v="24.0.0.0"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"POCO M2 Pro"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"12.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
+    'viewport-width': '980',
+}
+                url = 'https://b-graph.facebook.com/auth/login'
                 po = requests.post(url,data=data,headers=headers).json()
                 if "session_key" in po:
                     uid = po['uid']
